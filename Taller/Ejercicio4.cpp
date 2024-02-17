bc8f3eec5a91890e70f73e428be46bceeb101236
CvrlosLucero <carloslucerotobar001@gmail.com>
2024-02-16 22:44:48 -0500

    Added code to Ejercicio4.cpp

--- Taller/Ejercicio4.cpp
+++ Taller/Ejercicio4.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main(){
 
-    
+    double salario, horasTrabajadas, horasExtras, pagoAdicional;
+
+    cout<<"Ingrese la cantidad de horas trabajadas: "<<endl;
+    cin>>horasTrabajadas;
+
+    if(horasTrabajadas<=35){
+        salario = horasTrabajadas * 40000;
+        cout<<"El salario semanal del trabajador es de: "<<salario;
+    }else{
+        horasExtras = horasTrabajadas - 35;
+        pagoAdicional = horasExtras * 45000;
+        salario = 1400000 + pagoAdicional;
+        cout<<"El salario semanal del trabajador es de: "<<salario;
+
+
+        
+    }
+    return 0;
 }
