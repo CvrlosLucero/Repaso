fe762cead066048058d2c8b64c0147a3c54f935e
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:37:27 -0500

    Added documentation

--- Repaso Ejercicios/caraysello.cpp
+++ Repaso Ejercicios/caraysello.cpp
@@ -5,27 +5,32 @@ using namespace std;
 
 int main(){
 
+    //Declaracion de variables
     int eleccion, rnd;
     char res;
 
+    //Ingreso de elección
     cout<<"Elije una: [1] Cara [2] Sello"<<endl;
     cin>>eleccion;
     
+    //Condicional para determinar si la elección es válida
     if (eleccion < 1 || eleccion > 2) {
       cout << "Elección no válida. Intente de nuevo." <<endl;
-    
-	return 0;
+	    return 0;
     }
 	
-	rnd = rand() % 2;
+    //Generación de número aleatorio
+	  rnd = rand() % 2;
 	
-	if (eleccion == 1) {
+    //Condicional para determinar el resultado
+	  if (eleccion == 1) {
       res = rnd ? 'S' : 'C';
     } else {
       res = rnd ? 'C' : 'S';
     }
     
-    cout << "Le tocó: " <<res<<endl;
+    //Impresión de resultado
+    cout<<"Le tocó: "<<res<<endl;
 
     return 0;
 }
