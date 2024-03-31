54b2d998d1a2c1f295ed767e56db8695e7e4f876
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:26:56 -0500

    Added documentation

--- Taller/Ejercicio7.cpp
+++ Taller/Ejercicio7.cpp
@@ -3,16 +3,20 @@
 using namespace std;
 
 int main() {
-  int num;
 
-    cout << "Ingrese el número para verificación: ";
-    cin >> num;
+    //Declaracion de variables
+    int num;
 
-  if (num % 2 == 0) {
-        cout << "El número " <<num<<" es par."<<endl;
-  } else {
-        cout << "El número " <<num<<" es impar."<<endl;
-  }
+    //Ingreso de número
+    cout<<"Ingrese el número para verificación: ";
+    cin>>num;
+
+    //Condicional para determinar si el número es par o impar
+    if(num % 2 == 0){
+        cout<<"El número "<<num<<" es par."<<endl;
+    }else{
+        cout<<"El número "<<num<<" es impar."<<endl;
+    }
 
   return 0;
 }
