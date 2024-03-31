0622bc138b0a6102d9a04749139cb8665ed3e4ce
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:28:49 -0500

    Added documentation

--- Repaso Ejercicios/abuela.cpp
+++ Repaso Ejercicios/abuela.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main(){
 
+    //Declaracion de variables
     float temp, res;
 
+    //Ingreso de temperatura en Fahrenheit
     cout<<"Ingrese la temperatura Fahrenheit en números: ";
     cin>>temp;
 
+    //Calculo de la temperatura en Centigrados
     res = (temp-32) /1.8;
 
+    //Impresion de resultado
     cout<<"Su temperatura en Centigrados es: "<<res<<endl;
 
     return 0;
