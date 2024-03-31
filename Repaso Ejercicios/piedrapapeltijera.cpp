c73c6c0aca6fbc19c28bd334729685785a98aa98
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 15:02:24 -0500

    Added documentation
    An a randomizer to the Machine's election.

--- Repaso Ejercicios/piedrapapeltijera.cpp
+++ Repaso Ejercicios/piedrapapeltijera.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
 
 int main() {
   
+  //Declaracion de vector de opciones
   vector<string> opciones = {"Piedra", "Papel", "Tijera"};
 
- 
+  //Declaracion de eleccion
   int eleccion;
   cout << "Elige tu opción (0: Piedra, 1: Papel, 2: Tijera): ";
   cin >> eleccion;
 
-  int maquinaEleccion = (eleccion + 1) % 3;
+  //Generacion de eleccion de la maquina
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> distr(0, 2);
+  int maquinaEleccion = distr(gen);
 
-  
+  //Impresion de elecciones
   cout << "Yo: " << opciones[eleccion] << endl;
   cout << "Máquina: " << opciones[maquinaEleccion] << endl;
 
- 
+  //Condicional para determinar el ganador
   if (eleccion == maquinaEleccion) {
     cout << "Empate!" << endl;
   } else if ((eleccion == 0 && maquinaEleccion == 2) ||
