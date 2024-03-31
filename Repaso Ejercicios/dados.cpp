12eaccfe9e63edeb107b99d482cc29c429e7bec1
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:40:34 -0500

    Added documentation

--- Repaso Ejercicios/dados.cpp
+++ Repaso Ejercicios/dados.cpp
@@ -3,23 +3,28 @@
 
 using namespace std;
 
-int main()
-{
-    srand(time(NULL));
+int main(){
+    
+  srand(time(NULL));
 
+  //Declaracion de variables
   int round = 1;
   bool next = true;
 
+  //Inicio del juego
   while (next) {
     
+    //Inicio de ronda
     cout << "** Round " << round << " **" << endl;
 
+    //Tirada de dados del jugador y PC
     int throwPlayer = (rand() % 6) + 1;
     cout << "Your throw: " << throwPlayer << endl;
     
     int throwPC = (rand() % 6) + 1;
     cout << "PC's throw: " << throwPC << endl;
 
+    //Determinar ganador de la ronda
     if (throwPlayer > throwPC) {
       cout << "You win!" << endl;
     } else if (throwPlayer < throwPC) {
@@ -30,6 +35,7 @@ int main()
 
     round++;
 
+    //Preguntar si desea continuar
     cout << "Do you want to continue? (Y/N): ";
     char answer;
     cin >> answer;
