0895e0baf32a3272e2a8bb73eb4bc1a83e223870
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:20:32 -0500

    Added documentation

--- Taller/Ejercicio4.cpp
+++ Taller/Ejercicio4.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main(){
 
+    //Declaracion de variables
     double salario, horasTrabajadas, horasExtras, pagoAdicional;
 
+    //Ingreso de horas trabajadas
     cout<<"Ingrese la cantidad de horas trabajadas: "<<endl;
     cin>>horasTrabajadas;
 
+    //Condicional para determinar el salario semanal teniendo en cuenta que la hora trabajada se paga a $40.000 y las horas extras a $45.000
     if(horasTrabajadas<=35){
         salario = horasTrabajadas * 40000;
         cout<<"El salario semanal del trabajador es de: "<<salario;
