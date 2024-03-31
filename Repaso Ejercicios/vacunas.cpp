2a90f6b212f4b03624d2967660606aeb5923cc0c
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 15:04:31 -0500

    Added documentation

--- Repaso Ejercicios/vacunas.cpp
+++ Repaso Ejercicios/vacunas.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 int main(){
 
+    //Declaracion de variables
     float pesobebe, mesesbebe, dosisvacuna;
 
+    //Ingreso de peso y edad del bebe
     cout<<"Ingrese el peso del bebé en Kg: "<<endl;
     cin>>pesobebe;
     cout<<"Ingrese la edad en meses del bebe: "<<endl;
     cin>>mesesbebe;
 
+    //Calculo de la dosis de la vacuna
     dosisvacuna = ((pesobebe+10)/(mesesbebe*10))*8;
 
-    cout<<"La dosis del bebe es de: "<<dosisvacuna;
+    //Impresion de la dosis de la vacuna
+    cout<<"La dosis del bebe es de: "<<dosisvacuna<<" mL."<<endl;
     return 0;
 }
