1510f7a874d426603bec3b58bb78b5f43340845d
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:55:13 -0500

    Added documentation

--- Repaso Ejercicios/Log_in.cpp
+++ Repaso Ejercicios/Log_in.cpp
@@ -2,23 +2,28 @@
 
 using namespace std;
 
+//Declaracion de constantes (Usuario y contraseña)
 const string USUARIO = "Test123";
 const string CONTRASENA = "gatitos001";
 
+//Funcion para verificar el login
 bool login(string username, string password) {
 	
     return (username == USUARIO && password == CONTRASENA);
 }
 
 int main() {
+    //Declaracion de variables
     string username, password;
 
+    //Ingreso de usuario y contraseña
     cout << "Digite su nombre de usuario: ";
     cin >> username;
 
     cout << "Digite su contraseña: ";
     cin >> password;
 
+    //Verificacion de login
     if (login(username, password)) {
         cout << "¡Bienvenido!, " << username << "!" << endl;
     } else {
