ed158a953f899a1e5c4ed023c65b3156a374dbd9
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:18:21 -0500

    Added documentation

--- Taller/Ejercicio2.cpp
+++ Taller/Ejercicio2.cpp
@@ -4,16 +4,21 @@ using namespace std;
 
 int main(){
 
+    //Declaracion de variables
     int plan;
     double valorEquipo, descuento, valorNeto;
 
+    //Ingreso de tipo de plan
     cout<<"Ingrese el tipo de plan que tiene (0 para prepago, 1 para postpago): "<<endl;
         cin>>plan;
 
+    //Condicional para determinar el descuento a realizar según el plan
     switch(plan){
         case 0:
+            //Ingreso valor del equipo
             cout<<"Ingrese el valor del equipo a comprar: "<<endl;
                 cin>>valorEquipo;
+            //Condicional para determinar el descuento a realizar segun el valor del equipo
             if(valorEquipo>100000 && valorEquipo<900000){
                 descuento = valorEquipo * 0.10;
                 valorNeto = valorEquipo - descuento;
@@ -28,8 +33,10 @@ int main(){
             }
           break;
         case 1:
+            //Ingreso valor del equipo
             cout<<"Ingrese el valor del equipo a comprar: "<<endl;
                 cin>>valorEquipo;
+            //Condicional para determinar el descuento a realizar segun el valor del equipo
             if(valorEquipo<500000){
                 descuento = valorEquipo * 0.15;
                 valorNeto = valorEquipo - descuento;
@@ -48,6 +55,7 @@ int main(){
             }
           break;
         default:
+            //Mensaje de error
             cout<<"Error. Plan no encontrado, intentelo de nuevo.";
           break;
     }
