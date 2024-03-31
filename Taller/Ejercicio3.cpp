75de6efa1e0b7fee4a3c236a4878b718a64e618e
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 14:18:47 -0500

    Added documentation

--- Taller/Ejercicio3.cpp
+++ Taller/Ejercicio3.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main(){
 
+    //Declaracion de variables
     double salario, aumentoSalario;
 
+    //Ingreso de salario
     cout<<"Ingrese el salario del trabajador: ";
     cin>>salario;
 
+    //Condicional para determinar el aumento de salario
     if(salario<1000000){
         aumentoSalario = salario * 1.15;
         cout<<"El nuevo salario del trabajador es de: "<<aumentoSalario<<endl;
